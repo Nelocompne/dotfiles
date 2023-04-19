94ff314ee580c77af59b84306f35b3815752b794
Minn <qaqmango@outlook.com>
2023-04-19 20:14:49 +0800

    🆙 2023-04-19

--- update.ps1
+++ update.ps1
@@ -1,6 +1,6 @@
-cp ~\.config\clash-verge\verge.yaml .config\clash-verge\verge.yaml
-cp ~\.config\clash-verge\config.yaml .config\clash-verge\config.yaml
-cp ~\.config\clash\cfw-settings.yaml .config\clash\cfw-settings.yaml
+# cp ~\.config\clash-verge\verge.yaml .config\clash-verge\verge.yaml
+# cp ~\.config\clash-verge\config.yaml .config\clash-verge\config.yaml
+# cp ~\.config\clash\cfw-settings.yaml .config\clash\cfw-settings.yaml
 # cp ~\.config\scoop\config.json .config\scoop\config.json
-cp ~\.config\powershell\user_profile.ps1 .config\powershell\user_profile.ps1
+# cp ~\.config\powershell\user_profile.ps1 .config\powershell\user_profile.ps1
 # cp ~\AppData\Roaming\Rime\weasel.custom.yaml .\Rime
